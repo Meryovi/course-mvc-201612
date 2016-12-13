fd4e998c312106555ccf86b32ffc21838ff70bd8
Meryovi De Dios <mjdios2406@gmail.com>
2016-12-13 18:19:09 -0400

    Clase del lunes 12 de dic.

---
+++ src/clases/clase1/Proyecto1/Proyecto1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Formulario(string nombre)
+        {
+            ViewBag.Nombre = nombre;
+
+            return View();
+        }
+    }
+}
